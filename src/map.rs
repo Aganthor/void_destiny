4cac53fbe17a5208e87144d1cd9fb5667069947d
Luc <psyluche@gmail.com>
2022-04-24 10:18:26 -0400

    Adding a map plugin. WIP!

--- src/map.rs
+++ src/map.rs
@@ -1,1 +1,21 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+pub struct MapPlugin;
+
+impl Plugin for MapPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_plugin(TilemapPlugin)
+            .add_startup_system(setup)
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut map_query: MapQuery,
+) {
+    let texture_handle = asset_server.load("assets/tiles/terrain_tiles.png");
+}
+
