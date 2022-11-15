bd5539013d781f8023dd34ffa5ade803101d4913
Luc Bergeron <psyluche@gmail.com>
2022-11-15 14:49:10 -0500

    Main updated to 0.9.

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,18 @@
 /// - Save world map size in a ron file
 /// 
 
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::PresentMode,
+};
 
 //use benimator::AnimationPlugin;
 
 mod constants;
 use constants::*;
 
-mod player;
-use player::*;
+//mod player;
+//use player::*;
 
 mod map;
 use map::*;
@@ -26,21 +29,24 @@ use map::*;
 fn main() {
     App::new()
         .insert_resource(ClearColor(BG_COLOR))
-        .insert_resource(WindowDescriptor {
-            title: "Void destiny - The roguelike game!".to_string(),
-            width: WINDOW_WIDTH,
-            height: WINDOW_HEIGHT,
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Void destiny - The roguelike game!".to_string(),
+                width: WINDOW_WIDTH,
+                height: WINDOW_HEIGHT,
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            },
             ..default()
-        })
-        .add_plugins(DefaultPlugins)
+        }))
         //.add_plugin(AnimationPlugin::default())
-        .add_plugin(PlayerPlugin)
+//        .add_plugin(PlayerPlugin)
         .add_plugin(MapPlugin)
         .add_startup_system(setup_camera)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn_bundle(Camera2dBundle::new_2d());
+    commands.spawn(Camera2dBundle::default());
 }
 
