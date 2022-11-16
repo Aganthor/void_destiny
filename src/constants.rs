c80d9c930cb2e077b616015d510d6c259509dcb9
Luc Bergeron <psyluche@gmail.com>
2022-11-16 10:39:40 -0500

    Corrected a typo.

--- src/constants.rs
+++ src/constants.rs
@@ -5,8 +5,8 @@ pub const WINDOW_WIDTH: f32 = 1024.0;
 pub const WINDOW_HEIGHT: f32 = 768.0;
 
 // Overworld map size
-pub const OVERWOLRD_SIZE_WIDTH: u32 = 16;
-pub const OVERWOLRD_SIZE_HEIGHT: u32 = 16;
+pub const OVERWORLD_SIZE_WIDTH: u32 = 16;
+pub const OVERWORLD_SIZE_HEIGHT: u32 = 16;
 
 // Colors
 pub const BG_COLOR: Color = Color::rgb(38. / 255., 20. / 255., 40. / 255.);
