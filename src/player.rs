8c4b0fbcb25923f98a6b7f1d94eb6c6eef200376
Luc Bergeron <psyluche@gmail.com>
2022-11-21 14:14:46 -0500

    Migrated to 0.9... But animation not working.

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,7 @@ use bevy::{
     prelude::*,
 };
 
-use benimator::FrameRate;
+use benimator::*;
 
 const ANIMATION_DURATION: u64 = 200;
 
@@ -21,21 +21,27 @@ enum Direction {
 #[derive(Component, Deref)]
 struct PlayerAnimation(benimator::Animation);
 
-
 #[derive(Default, Component, Deref, DerefMut)]
 struct PlayerAnimationState(benimator::State);
 
-#[derive(Resource, Default)]
-struct PlayerSpriteHandles {
-    handles: Vec<HandleUntyped>,
-}
-
-#[derive(Resource, Default)]
+#[derive(Resource)]
 struct DirectionAnimations {
     up: benimator::Animation,
     down: benimator::Animation,
     left: benimator::Animation,
     right: benimator::Animation,
+}
+
+impl Default for DirectionAnimations {
+    fn default() -> Self {
+        let anim_duration = Duration::new(ANIMATION_DURATION, 0);
+         DirectionAnimations { 
+            up: Animation::from_indices(9..=11, FrameRate::from_total_duration(anim_duration)), 
+            down: Animation::from_indices(0..=2, FrameRate::from_total_duration(anim_duration)), 
+            left: Animation::from_indices(3..=5, FrameRate::from_total_duration(anim_duration)), 
+            right: Animation::from_indices(6..=8, FrameRate::from_total_duration(anim_duration)),
+        }
+    }
 }
 
 #[derive(Default)]
@@ -44,45 +50,47 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_startup_system_to_stage(StartupStage::PreStartup, prepare_player_animations)
+            .init_resource::<DirectionAnimations>()
             .add_startup_system(setup)
-            .add_system(move_player);
+            .add_system(move_player)
+            .add_system(animate_player);
     }
+}
+
+fn animate_player(
+    time: Res<Time>,
+    mut animation_query: Query<(&PlayerAnimation, &mut PlayerAnimationState, &mut TextureAtlasSprite)>,
+) {
+    for (animation, mut animation_state, mut texture_atlas) in &mut animation_query {
+        animation_state.update(&animation, time.delta());
+        texture_atlas.index = animation_state.frame_index();
+    } 
 }
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
-    animations: Res<PlayerAnimations>,
+    animations: Res<DirectionAnimations>,
     time: Res<Time>,
-    mut query: Query<(&mut Handle<SpriteSheetAnimation>, &mut Transform)>,
+    mut direction_query: Query<(&mut PlayerAnimation, &mut Transform)>,
 ) {
-    let (mut animation, mut transform) = query.single_mut();
+    let (mut animation, mut transform) = direction_query.single_mut();
 
     if keyboard_input.pressed(KeyCode::A) {
-        *animation = animations.left.clone();
+        *animation = PlayerAnimation(animations.left.clone());
         transform.translation.x -= 100. * time.delta_seconds();
     } else if keyboard_input.pressed(KeyCode::D) {
-        *animation = animations.right.clone();
+        *animation = PlayerAnimation(animations.right.clone());
         transform.translation.x += 100. * time.delta_seconds();
     } else if keyboard_input.pressed(KeyCode::S) {
-        *animation = animations.down.clone();
+        *animation = PlayerAnimation(animations.down.clone());
         transform.translation.y -= 100. * time.delta_seconds();
     } else if keyboard_input.pressed(KeyCode::W) {            
-        *animation = animations.up.clone();
+        *animation = PlayerAnimation(animations.up.clone());
         transform.translation.y += 100. * time.delta_seconds();
     } else if keyboard_input.any_just_released([KeyCode::A, KeyCode::S, KeyCode::D, KeyCode::W]) {
-        *animations = animations.standing.clone();
+        //*animations = animations.standing.clone();
+        println!("Just released a key... stop animation.")
     }
-}
-
-fn prepare_player_animations(
-    mut direction_animations: ResMut<DirectionAnimations>,
-    asset_server: Res<AssetServer>
-) {
-    direction_animations.up = Animation(benimator::Animation::from_indices(9..=11, FrameRate::from_total_duration(ANIMATION_DURATION)));
-    direction_animations.down = Animation(benimator::Animation::from_indices(0..=2, FrameRate::from_total_duration(ANIMATION_DURATION)));
-    direction_animations.left = Animation(benimator::Animation::from_indices(3..=5, FrameRate::from_total_duration(ANIMATION_DURATION)));
-    direction_animations.right = Animation(benimator::Animation::from_indices(6..=8, FrameRate::from_total_duration(ANIMATION_DURATION)));    
 }
 
 fn setup(
@@ -102,12 +110,12 @@ fn setup(
     let texture_atlas_handle = texture_atlases.add(texture_atlas); 
 
     commands
-        .spawn_bundle(SpriteSheetBundle {
+        .spawn(SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             transform: Transform::from_scale(Vec3::splat(1.0)),
             ..default()
         })
-        .insert(PlayerAnimation::direction_animations.left)
+        .insert(PlayerAnimation(direction_animations.left.clone()))
         .insert(PlayerAnimationState::default())
         .insert(Direction::Left);
 }
