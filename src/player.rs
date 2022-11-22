957e956e8ac95560f59e4c323f3175df7f647527
Luc Bergeron <psyluche@gmail.com>
2022-11-22 14:43:26 -0500

    Trying to slap the player to the map grid.

--- src/player.rs
+++ src/player.rs
@@ -8,6 +8,8 @@ use bevy::{
 
 use benimator::*;
 
+use crate::constants::{OVERWORLD_SIZE_WIDTH, OVERWORLD_SIZE_HEIGHT};
+
 const ANIMATION_DURATION: u64 = 200;
 
 #[derive(Component)]
@@ -45,6 +47,27 @@ impl Default for DirectionAnimations {
     }
 }
 
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct PlayerPlugin;
 
@@ -56,6 +79,12 @@ impl Plugin for PlayerPlugin {
                 SystemSet::new()
                     .with_system(move_player)
                     .with_system(animate_player.after(move_player)),
+            )
+            .add_system_set_to_stage(
+                CoreStage::PostUpdate,
+                SystemSet::new()
+                    .with_system(position_translation)   
+                    .with_system(size_scaling),
             );
     }
 }
@@ -71,7 +100,6 @@ fn animate_player(
     for (animation, mut animation_state, mut texture_atlas) in &mut animation_query {
         animation_state.update(animation, time.delta());
         texture_atlas.index = animation_state.frame_index();
-        println!("Texture index in atlas = {}", texture_atlas.index);
     }
 }
 
@@ -112,15 +140,44 @@ fn setup(
         TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 3, 4, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     let player_position = Transform::from_translation(Vec3::Z * 10.0) * Transform::from_scale(Vec3::splat(1.0));
-    
+
     commands
         .spawn(SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             transform: player_position,
-            
             ..default()
         })
         .insert(PlayerAnimation(direction_animations.left.clone()))
         .insert(PlayerAnimationState::default())
-        .insert(Direction::Left);
+        .insert(Direction::Left)
+        .insert(Position { x: 0, y: 0 })
+        .insert(Size::square(0.5));
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / OVERWORLD_SIZE_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / OVERWORLD_SIZE_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, OVERWORLD_SIZE_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, OVERWORLD_SIZE_HEIGHT as f32),
+            0.0,
+        );
+    }
 }
