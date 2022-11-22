f5525502a934106ad48ba3e603aaed389d72fca3
Luc Bergeron <psyluche@gmail.com>
2022-11-22 16:09:34 -0500

    Fixed scaling error.

--- src/player.rs
+++ src/player.rs
@@ -3,7 +3,6 @@ use core::time::Duration;
 use bevy::{
     input::{keyboard::KeyCode, Input},
     prelude::*,
-    render::texture,
 };
 
 use benimator::*;
@@ -53,20 +52,20 @@ struct Position {
     y: i32,
 }
 
-#[derive(Component)]
-struct Size {
-    width: f32,
-    height: f32,
-}
+// #[derive(Component)]
+// struct Size {
+//     width: f32,
+//     height: f32,
+// }
 
-impl Size {
-    pub fn square(x: f32) -> Self {
-        Self {
-            width: x,
-            height: x,
-        }
-    }
-}
+// impl Size {
+//     pub fn square(x: f32) -> Self {
+//         Self {
+//             width: x,
+//             height: x,
+//         }
+//     }
+// }
 
 #[derive(Default)]
 pub struct PlayerPlugin;
@@ -84,7 +83,7 @@ impl Plugin for PlayerPlugin {
                 CoreStage::PostUpdate,
                 SystemSet::new()
                     .with_system(position_translation)   
-                    .with_system(size_scaling),
+            //       .with_system(size_scaling),
             );
     }
 }
@@ -107,22 +106,27 @@ fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
     animations: Res<DirectionAnimations>,
     time: Res<Time>,
-    mut direction_query: Query<(&mut PlayerAnimation, &mut Transform)>,
+    //mut direction_query: Query<(&mut PlayerAnimation, &mut Transform)>,
+    mut direction_query: Query<(&mut PlayerAnimation, &mut Position)>,
 ) {
-    let (mut animation, mut transform) = direction_query.single_mut();
+    let (mut animation, mut position) = direction_query.single_mut();
 
     if keyboard_input.pressed(KeyCode::A) {
         *animation = PlayerAnimation(animations.left.clone());
-        transform.translation.x -= 100. * time.delta_seconds();
+        //transform.translation.x -= 100. * time.delta_seconds();
+        position.x -= 1;
     } else if keyboard_input.pressed(KeyCode::D) {
         *animation = PlayerAnimation(animations.right.clone());
-        transform.translation.x += 100. * time.delta_seconds();
+        //transform.translation.x += 100. * time.delta_seconds();
+        position.x += 1;
     } else if keyboard_input.pressed(KeyCode::S) {
         *animation = PlayerAnimation(animations.down.clone());
-        transform.translation.y -= 100. * time.delta_seconds();
+        //transform.translation.y -= 100. * time.delta_seconds();
+        position.y -= 1;
     } else if keyboard_input.pressed(KeyCode::W) {
         *animation = PlayerAnimation(animations.up.clone());
-        transform.translation.y += 100. * time.delta_seconds();
+        //transform.translation.y += 100. * time.delta_seconds();
+        position.y += 1;
     } else if keyboard_input.any_just_released([KeyCode::A, KeyCode::S, KeyCode::D, KeyCode::W]) {
         //*animations = animations.standing.clone();
         println!("Just released a key... stop animation.")
@@ -150,23 +154,26 @@ fn setup(
         .insert(PlayerAnimation(direction_animations.left.clone()))
         .insert(PlayerAnimationState::default())
         .insert(Direction::Left)
-        .insert(Position { x: 0, y: 0 })
-        .insert(Size::square(0.5));
+        .insert(Position { x: 0, y: 0 });
+        //.insert(Size::square(0.08));
 }
 
-fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
-    let window = windows.get_primary().unwrap();
+// fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+//     let window = windows.get_primary().unwrap();
 
-    for (sprite_size, mut transform) in q.iter_mut() {
-        transform.scale = Vec3::new(
-            sprite_size.width / OVERWORLD_SIZE_WIDTH as f32 * window.width() as f32,
-            sprite_size.height / OVERWORLD_SIZE_HEIGHT as f32 * window.height() as f32,
-            1.0,
-        );
-    }
-}
+//     for (sprite_size, mut transform) in q.iter_mut() {
+//         transform.scale = Vec3::new(
+//             sprite_size.width / OVERWORLD_SIZE_WIDTH as f32 * window.width() as f32,
+//             sprite_size.height / OVERWORLD_SIZE_HEIGHT as f32 * window.height() as f32,
+//             10.0,
+//         );
+//     }
+// }
 
-fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+fn position_translation(
+    windows: Res<Windows>, 
+    mut q: Query<(&Position, &mut Transform)>, 
+) {
     fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
         let tile_size = bound_window / bound_game;
         pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
@@ -177,7 +184,7 @@ fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Tra
         transform.translation = Vec3::new(
             convert(pos.x as f32, window.width() as f32, OVERWORLD_SIZE_WIDTH as f32),
             convert(pos.y as f32, window.height() as f32, OVERWORLD_SIZE_HEIGHT as f32),
-            0.0,
+            10.0,
         );
     }
 }
