7994d151b9b589879bd5f7b06a6f17d91cf06589
Luc Bergeron <psyluche@gmail.com>
2022-11-22 13:51:36 -0500

    Player is displayed, but no animation.

--- src/player.rs
+++ src/player.rs
@@ -3,6 +3,7 @@ use core::time::Duration;
 use bevy::{
     input::{keyboard::KeyCode, Input},
     prelude::*,
+    render::texture,
 };
 
 use benimator::*;
@@ -18,7 +19,7 @@ enum Direction {
     Standing,
 }
 
-#[derive(Component, Deref)]
+#[derive(Component, Deref, Debug)]
 struct PlayerAnimation(benimator::Animation);
 
 #[derive(Default, Component, Deref, DerefMut)]
@@ -35,11 +36,11 @@ struct DirectionAnimations {
 impl Default for DirectionAnimations {
     fn default() -> Self {
         let anim_duration = Duration::new(ANIMATION_DURATION, 0);
-         DirectionAnimations { 
-            up: Animation::from_indices(9..=11, FrameRate::from_total_duration(anim_duration)), 
-            down: Animation::from_indices(0..=2, FrameRate::from_total_duration(anim_duration)), 
-            left: Animation::from_indices(3..=5, FrameRate::from_total_duration(anim_duration)), 
-            right: Animation::from_indices(6..=8, FrameRate::from_total_duration(anim_duration)),
+        DirectionAnimations {
+            up: Animation::from_indices(9..=11, FrameRate::from_frame_duration(anim_duration)),
+            down: Animation::from_indices(0..=2, FrameRate::from_frame_duration(anim_duration)),
+            left: Animation::from_indices(3..=5, FrameRate::from_frame_duration(anim_duration)),
+            right: Animation::from_indices(6..=8, FrameRate::from_frame_duration(anim_duration)),
         }
     }
 }
@@ -47,41 +48,31 @@ impl Default for DirectionAnimations {
 #[derive(Default)]
 pub struct PlayerPlugin;
 
-
-// .add_system_set(
-//     SystemSet::new()
-//         // This label is added to all systems in this set.
-//         // The label can then be referred to elsewhere (other sets).
-//         .label(Physics)
-//         // This criteria ensures this whole system set only runs when this system's
-//         // output says so (ShouldRun::Yes)
-//         .with_run_criteria(run_for_a_second)
-//         .with_system(update_velocity)
-//         // Make movement run after update_velocity
-//         .with_system(movement.after(update_velocity)),
-// )
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .init_resource::<DirectionAnimations>()
+        app.init_resource::<DirectionAnimations>()
             .add_startup_system(setup)
             .add_system_set(
                 SystemSet::new()
                     .with_system(move_player)
-                    .with_system(animate_player.after(move_player)));
-//            .add_system(move_player)
-//            .add_system(animate_player);
+                    .with_system(animate_player.after(move_player)),
+            );
     }
 }
 
 fn animate_player(
     time: Res<Time>,
-    mut animation_query: Query<(&PlayerAnimation, &mut PlayerAnimationState, &mut TextureAtlasSprite)>,
+    mut animation_query: Query<(
+        &PlayerAnimation,
+        &mut PlayerAnimationState,
+        &mut TextureAtlasSprite,
+    )>,
 ) {
     for (animation, mut animation_state, mut texture_atlas) in &mut animation_query {
         animation_state.update(animation, time.delta());
         texture_atlas.index = animation_state.frame_index();
-    } 
+        println!("Texture index in atlas = {}", texture_atlas.index);
+    }
 }
 
 fn move_player(
@@ -101,7 +92,7 @@ fn move_player(
     } else if keyboard_input.pressed(KeyCode::S) {
         *animation = PlayerAnimation(animations.down.clone());
         transform.translation.y -= 100. * time.delta_seconds();
-    } else if keyboard_input.pressed(KeyCode::W) {            
+    } else if keyboard_input.pressed(KeyCode::W) {
         *animation = PlayerAnimation(animations.up.clone());
         transform.translation.y += 100. * time.delta_seconds();
     } else if keyboard_input.any_just_released([KeyCode::A, KeyCode::S, KeyCode::D, KeyCode::W]) {
@@ -117,14 +108,9 @@ fn setup(
     direction_animations: Res<DirectionAnimations>,
 ) {
     let texture_handle = asset_server.load("Male 01-1.png");
-    let texture_atlas = TextureAtlas::from_grid(
-        texture_handle, 
-        Vec2::new(32.0, 32.0), 
-        3, 
-        4,
-        None,
-        None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas); 
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 3, 4, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     commands
         .spawn(SpriteSheetBundle {
