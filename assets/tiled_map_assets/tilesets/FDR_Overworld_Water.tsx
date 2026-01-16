01e170d828ead5b94f3aadf8a38d50d730e6c8d6
Aganthor <psyluche@gmail.com>
2026-01-16 14:52:00 -0500

    Changed maps.

--- assets/tiled_map_assets/tilesets/FDR_Overworld_Water.tsx
+++ assets/tiled_map_assets/tilesets/FDR_Overworld_Water.tsx
@@ -25,4 +25,40 @@
    <frame tileid="94" duration="100"/>
   </animation>
  </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="92" duration="100"/>
+   <frame tileid="95" duration="100"/>
+  </animation>
+ </tile>
+ <wangsets>
+  <wangset name="Water" type="corner" tile="-1">
+   <wangcolor name="Water" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="12" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="14" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="24" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="25" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="26" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="36" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="37" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="38" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="48" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="50" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="60" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="62" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="72" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="74" wangid="0,0,0,1,0,0,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
