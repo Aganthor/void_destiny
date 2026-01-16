8f4e5adde75bcad9e8abd7ad4bbac129be15ce5d
Aganthor <psyluche@gmail.com>
2026-01-16 10:45:17 -0500

    Added the tiled map assets folder.

---
+++ assets/tiled_map_assets/tilesets/FDR_Overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="FDR_Overworld" tilewidth="16" tileheight="16" tilecount="704" columns="32">
+ <image source="FDR_Overworld.png" trans="ff00ff" width="512" height="352"/>
+</tileset>
---
+++ assets/tiled_map_assets/tilesets/FDR_Overworld_Ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="FDR_Overworld_Ground" tilewidth="16" tileheight="16" tilecount="630" columns="30">
+ <image source="FDR_Overworld_Ground.png" trans="ff00ff" width="480" height="336"/>
+</tileset>
