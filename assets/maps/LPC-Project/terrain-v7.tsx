0d60cf567469807a94760c0312cb839053dffe3e
Aganthor <psyluche@gmail.com>
2026-01-23 12:31:55 -0500

    Modified the map.

---
+++ assets/maps/LPC-Project/terrain-v7.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain-v7" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
+ <image source="../../../../../assets/lpc-terrains/lpc-terrains/terrain-v7.png" trans="ff00ff" width="1024" height="2048"/>
+ <tile id="611" probability="0.25"/>
+ <tile id="612" probability="0.25"/>
+ <tile id="613" probability="0.25"/>
+ <wangsets>
+  <wangset name="terrain" type="corner" tile="-1">
+   <wangcolor name="water" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="452" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="453" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="484" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="485" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="515" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="516" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="517" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="547" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="548" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="549" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="579" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="580" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="581" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="611" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="612" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="613" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="643" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="644" wangid="0,0,0,1,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
