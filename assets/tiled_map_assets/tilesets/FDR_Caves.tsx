242d1a64faacc06e52c292404cf4f3264b887fd8
Aganthor <psyluche@gmail.com>
2026-04-02 16:18:23 -0400

    Work on cave map.

--- assets/tiled_map_assets/tilesets/FDR_Caves.tsx
+++ assets/tiled_map_assets/tilesets/FDR_Caves.tsx
@@ -1,16 +1,32 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="FDR_Caves" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="FDR_Caves.png" trans="ff00ff" width="512" height="512"/>
+ <tile id="7" probability="0.33"/>
+ <tile id="8" probability="0.5"/>
  <tile id="9" probability="0.5"/>
  <wangsets>
   <wangset name="cave" type="corner" tile="8">
-   <wangcolor name="elevation" color="#ff0000" tile="-1" probability="1"/>
    <wangcolor name="floor" color="#00ff00" tile="-1" probability="0.5"/>
-   <wangcolor name="outer_walls" color="#0000ff" tile="-1" probability="1"/>
-   <wangcolor name="walls" color="#ff7700" tile="-1" probability="1"/>
-   <wangcolor name="inner_walls" color="#00e9ff" tile="-1" probability="1"/>
-   <wangtile tileid="8" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="9" wangid="0,2,0,2,0,2,0,2"/>
+   <wangcolor name="floor-deco" color="#413aff" tile="-1" probability="1"/>
+   <wangtile tileid="7" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="8" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="325" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="326" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="327" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="328" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="357" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="358" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="359" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="360" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="389" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="390" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="391" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="392" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="421" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="422" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="423" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="424" wangid="0,2,0,2,0,2,0,2"/>
   </wangset>
  </wangsets>
 </tileset>
